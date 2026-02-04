1652e99c42e048efc761aec4a440248f1758a718
timleung10 <timleung10@gmail.com>
2026-02-05 00:58:02 +0800

    fix base path and PWA scope for pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  base: "/taskmedic-a2.0/",
   plugins: [
     VitePWA({
       registerType: "autoUpdate",
