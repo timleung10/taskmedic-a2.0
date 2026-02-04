3339128d8b5e13e47bccd7df395edb0993a20ca2
timleung10 <timleung10@gmail.com>
2026-02-05 01:44:49 +0800

    fix pages, use relative base for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  base: "/taskmedic-a2.0/",
+  base: "./",
   plugins: [
     VitePWA({
       registerType: "autoUpdate",
